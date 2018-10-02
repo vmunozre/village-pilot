772bc0a2f9bc12efce2d8ee05f726602808ae4b6
vmunozre <v.munozre@alumnos.urjc.es>
2018-10-02 21:17:43 +0200

    Added bush and warehouse scripts

---
+++ Assets/Scripts/GameData/Entities/BushEntity.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BushEntity : MonoBehaviour {
+    public int age = 1;
+    public int food = 100;
+    public int rare = 0;
+    // Timer
+    float timer = 0f;
+    float waitTime = 10f;
+    // Use this for initialization
+    void Start () {
+        rare = Random.Range(10, 20);
+        waitTime += rare;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (age < 15)
+        {
+            timer += Time.deltaTime;
+            if (timer > waitTime)
+            {
+                food += 20;
+                age += 1;
+                print("Bush grew, age: " + age.ToString());
+                timer = 0f;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GameData/Entities/WarehouseEntity.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarehouseEntity : MonoBehaviour {
+    public int wood = 0;
+    public int food = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void addFood(int _food)
+    {
+        food += _food;
+    }
+
+    public void addWood(int _wood)
+    {
+        wood += _wood;
+    }
+
+    public void removeFood(int _food)
+    {
+        food -= _food;
+    }
+
+    public void removeWood(int _wood)
+    {
+        wood -= _wood;
+    }
+}
